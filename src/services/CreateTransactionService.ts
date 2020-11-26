e0b38aaa1b3ffd0b519d0eb23ab1a80d14501a0a
Natan Barbosa <natanbarbosa15@gmail.com>
2020-11-25 22:19:19 -0300

    Desafio concluído.

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,27 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type == 'outcome' && total - value < 0) {
+      throw new Error('Não há saldo suficiente para a transação.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
